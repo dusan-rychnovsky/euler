1643e763aeb6daeb1e69994ec18bb6154cf16105
Dusan Rychnovsky <dusan.rychnovsky@gmail.com>
2014-12-06 19:22:07 +0100

    Add a solution for problem 05 in Ruby.

---
+++ 05/multiple.rb
@@ -0,0 +1,9 @@
+# Smallest multiple
+# Problem 5
+#
+# 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder. What is the
+# smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+#
+
+p 16 * 9 * 5 * 7 * 11 * 13 * 17 * 19  # the least common multiple of numbers [1..20]
+p "DONE"
