b37123dfc54747460f17b0887e555d9dc8a2a33f
Dusan Rychnovsky <dusan.rychnovsky@gmail.com>
2014-12-07 12:35:07 +0100

    Optimize the solution for 10 in Ruby - TOO SLOW STILL.

--- 10/prime.rb
+++ 10/prime.rb
@@ -4,12 +4,23 @@
 # The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17. Find the sum of all the primes below two million.
 #
 
+def drop_while! arr, &block
+  unless arr.empty?
+    x = arr.first
+    if block.call(x)
+      arr.shift
+      drop_while! arr, &block
+    end
+  end
+end
+
 def sieve_once arr, p
-  arr.reject! { |x| x % p == 0 }
+  arr.map! { |x| x % p == 0 ? -1 : x }
 end
 
 def sieve_rec arr
-  if arr.empty?
+  drop_while!(arr) { |x| x == -1 }
+  if arr.empty? then
     []
   else
     p = arr.shift
