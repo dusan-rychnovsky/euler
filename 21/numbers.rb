8e4f1778d9ec2c815b071ff1a84a6f84f885d7ea
Dusan Rychnovsky <dusan.rychnovsky@gmail.com>
2015-01-03 14:03:11 +0100

    21 in Ruby

---
+++ 21/numbers.rb
@@ -0,0 +1,26 @@
+# Amicable numbers
+# Problem 21
+#
+# Let d(n) be defined as the sum of proper divisors of n (numbers less than n which divide evenly into n). If d(a) = b
+# and d(b) = a, where a ≠ b, then a and b are an amicable pair and each of a and b are called amicable numbers.
+#
+# For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110; therefore d(220) = 284. The
+# proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
+#
+# Evaluate the sum of all the amicable numbers under 10000.
+#
+
+def proper_divisors n
+  (1..n-1).select { |d| n % d == 0 }
+end
+
+def d n
+  proper_divisors(n).inject(:+)
+end
+
+def amicable? n
+  (d(n) != n) && (d(d(n)) == n)
+end
+
+p (2..10000).select { |x| amicable?(x) }.inject(:+)
+p "DONE"
